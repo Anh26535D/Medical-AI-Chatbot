4e960bde084ecd59f14061a540ad12e4613e3537
Anh26535D <86581717+Anh26535D@users.noreply.github.com>
2026-04-05 09:08:09 +0700

    chore: add navigation and icons dependencies and define brand colors

---
+++ app/src/main/java/edu/hust/medicalaichatbot/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package edu.hust.medicalaichatbot.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val PrimaryBlue = Color(0xFF1589D9)
+val LightBlue = Color(0xFFE3F2FD)
+val TextGray = Color(0xFF757575)
+val BackgroundGradientStart = Color(0xFFE0F7FA)
+val BackgroundGradientEnd = Color(0xFFFFFFFF)
+val SuccessGreen = Color(0xFF4CAF50)
